dec350eddd0d245d06d697f83dbf06294b26bab0
Alexandre Segers <alexandre.segers@ugent.be>
2023-09-13 17:04:31 +0200

    Readme changes

--- R/data.R
+++ R/data.R
@@ -3,17 +3,14 @@
 #' (https://bioconductor.org/packages/release/bioc/html/ASpli.html,
 #' 10.1093/bioinformatics/btab141).
 #' @title saseRExample
-#' @return Loads all data needed to run vignette and examples
+#' @return Loads all data needed to run examples
 #' @usage data(saseRExample)
 NULL
 
 
 #' @name ASpliSE
-#' @title saseRExample
-#' @description saseRExample
-#' @format saseRExample
-#' @source saseRExample
-#' @references aa
+#' @title ASpliSE
+#' @description SummarizedExperiment containing gene counts, bin counts and junction counts in the metadata slots, together with some rowData information. Output from BamtoAspliCounts.
 #' @return Loads all data needed to run vignette and examples
 #' @usage data(saseRExample)
 NULL
@@ -21,40 +18,31 @@ NULL
 
 #' @name features
 #' @title features
-#' @description features
-#' @format features
-#' @source features
-#' @references aa
+#' @description features used to count bam-files. These are obtained by binGenome from ASpli.
+#' @format /
+#' @source https://doi.org/10.1093/bioinformatics/btab141 
+#' @references Estefania Mancini, Andres Rabinovich, Javier Iserte, Marcelo Yanovsky, Ariel Chernomoretz, ASpli: integrative analysis of splicing landscapes through RNA-Seq assays, Bioinformatics, Volume 37, Issue 17, September 2021, Pages 2609–2616, https://doi.org/10.1093/bioinformatics/btab141
 #' @return Loads all data needed to run vignette and examples
 #' @usage data(saseRExample)
 NULL
 
 #' @name SEgenes
 #' @title SEgenes
-#' @description SEgenes
-#' @format SEgenes
-#' @source SEgenes
-#' @references SEgenes
+#' @description SummarizedExperiment containing gene-level counts to perform an example aberrant expression analysis.
 #' @return Loads all data needed to run vignette and examples
 #' @usage data(saseRExample)
 NULL
 
 #' @name SEbins
 #' @title SEbins
-#' @description SEbins
-#' @format SEbins
-#' @source SEbins
-#' @references SEbins
+#' @description SummarizedExperiment containing bin-level counts to perform an example aberrant splicing analysis.
 #' @return Loads all data needed to run vignette and examples
 #' @usage data(saseRExample)
 NULL
 
 #' @name SEjunctions
 #' @title SEjunctions
-#' @description SEjunctions
-#' @format SEjunctions
-#' @source SEjunctions
-#' @references SEjunctions
+#' @description SummarizedExperiment containing junction-level counts to perform an example aberrant splicing analysis
 #' @return Loads all data needed to run vignette and examples
 #' @usage data(saseRExample)
 NULL
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # saseR
 
-saseR is a highly performant and fast algorithm to perform aberrant expression and splicing analyses.
+saseR is a highly performant and fast algorithm to perform aberrant expression and splicing analyses. By using adapted offsets in the negative binomial framework, we have shown that aberrant and differential splicing can be modelled. Next, by using an adapted mean-variance structure, we get unbiased estimators that can be used when many latent confounders are included, which is often the case for rare Mendelian studies. With this combination of adapted offsets and fast parameter estimation, we have shown that we are much faster compared to the state-of-the-art aberrant expression and splicing algorithms, while outperforming the latter to detect aberrant splicing events.
+
+For more information, be sure to check out our bioRxiv preprint and the vignette of this package: 
+https://doi.org/10.1101/2023.06.29.547014
 
 ## NEWS
 
 ## Installation instructions
-
 
 To install the development version, run;
 
@@ -18,7 +20,13 @@ The dependencies of the package are listed in the DESCRIPTION file of the packag
 
 saseR will be soon submitted to Bioconductor.
 
+
 ## Issues and bug reports
 
 Please use https://github.com/statOmics/saseR/issues to submit issues, bug reports, and comments.
 
+# Example data
+
+Example data was obtained from the ASpli package.
+
+Estefania Mancini, Andres Rabinovich, Javier Iserte, Marcelo Yanovsky, Ariel Chernomoretz, ASpli: integrative analysis of splicing landscapes through RNA-Seq assays, Bioinformatics, Volume 37, Issue 17, September 2021, Pages 2609–2616, https://doi.org/10.1093/bioinformatics/btab141
